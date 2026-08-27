dbe52f10e2236f3cff84d9e6765f293000ce9edf
Fuzz07 <davealagban9tle@gmail.com>
2026-08-27 23:18:18 +0800

    dunosasa

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "com.cctn.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations += setOf("en")
