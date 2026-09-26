0a05547271bf37f801d6ec229ada21c53a8e3c33
Fuzz07 <davealagban9tle@gmail.com>
2026-09-26 22:47:47 +0800

    djvgnlk

--- android-app/app/src/main/java/com/cctn/app/ui/screens/auth/RegisterScreen.kt
+++ android-app/app/src/main/java/com/cctn/app/ui/screens/auth/RegisterScreen.kt
@@ -46,7 +46,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
